814265c2a84e91a04f932e883972efb429d94556
Alex Whalen <awhalen1999@Gmail.com>
2024-02-16 01:31:39 -0330

    note

--- src/KingsCup.jsx
+++ src/KingsCup.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import placeholderImage from './assets/red.png';
 import rules from './Text';
 
+// re-add rule for ace of spades
+
 const DeckOfCards = () => {
   const initialDeck = [
     '2H',
