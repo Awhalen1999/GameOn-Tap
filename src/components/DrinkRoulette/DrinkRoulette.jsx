78897815167e207428f88cef45864d89868de415
Alex Whalen <awhalen1999@Gmail.com>
2024-02-20 15:16:31 -0330

    new circle plus spin working using default css

--- src/components/DrinkRoulette/DrinkRoulette.jsx
+++ src/components/DrinkRoulette/DrinkRoulette.jsx
@@ -1,73 +1,64 @@
 import React from 'react';
 import './DrinkRoulette.css';
 
-const DrinkRoulette = () => {
-  return (
-    <div>
-      <ul className='circle'>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            1
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            2
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            3
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            4
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            5
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            6
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            7
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            8
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            9
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            10
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            11
-          </div>
-        </li>
-        <li>
-          <div className='text' contentEditable='true' spellcheck='false'>
-            12
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class DrinkRoulette extends React.Component {
+  state = {
+    name: 'circle',
+  };
+
+  startRotation = () => {
+    this.setState({ name: 'circle start-rotate' });
+    setTimeout(() => this.setState({ name: 'circle' }), 2000);
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='arrow'></div>
+        <ul className={this.state.name}>
+          <li>
+            <div className='text'>1</div>
+          </li>
+          <li>
+            <div className='text'>2</div>
+          </li>
+          <li>
+            <div className='text'>3</div>
+          </li>
+          <li>
+            <div className='text'>4</div>
+          </li>
+          <li>
+            <div className='text'>5</div>
+          </li>
+          <li>
+            <div className='text'>6</div>
+          </li>
+          <li>
+            <div className='text'>7</div>
+          </li>
+          <li>
+            <div className='text'>8</div>
+          </li>
+          <li>
+            <div className='text'>9</div>
+          </li>
+          <li>
+            <div className='text'>10</div>
+          </li>
+          <li>
+            <div className='text'>11</div>
+          </li>
+          <li>
+            <div className='text'>12</div>
+          </li>
+        </ul>
+        <button className='spin-button' onClick={this.startRotation}>
+          SPIN
+        </button>
+      </div>
+    );
+  }
+}
 
 export default DrinkRoulette;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   },
   theme: {
-    extend: {},
+    extend: {
+      skew: {
+        '-60': '-60deg',
+      },
+      rotate: {
+        0: '0deg',
+        30: '30deg',
+        60: '60deg',
+        90: '90deg',
+        120: '120deg',
+        150: '150deg',
+        180: '180deg',
+        210: '210deg',
+        240: '240deg',
+        270: '270deg',
+        300: '300deg',
+        330: '330deg',
+      },
+    },
   },
   plugins: [],
 };
