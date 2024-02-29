995d6b0ad831013b1de375c8573e2a7b589513cf
Alex Whalen <awhalen1999@Gmail.com>
2024-02-29 20:11:56 -0330

    gh pages working

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://awhalen1999.github.io/',
   base: '/GameOn-Tap/',
 });
