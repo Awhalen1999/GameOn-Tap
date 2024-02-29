e5754480e1764c1aad8da76fd736355716dfbb77
Alex Whalen <awhalen1999@Gmail.com>
2024-02-29 19:56:07 -0330

    *

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // base: 'https://awhalen1999.github.io/',
-  base: '/GameOn-Tap/',
+  // base: '/GameOn-Tap/',
 });
