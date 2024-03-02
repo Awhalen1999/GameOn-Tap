88e0f6dd707547c8cd01a05fa5cb55cf76aba030
Alex Whalen <awhalen1999@Gmail.com>
2024-03-01 23:09:52 -0330

    rule descriptions

--- src/Pages/Games/Snap/SnapRules.js
+++ src/Pages/Games/Snap/SnapRules.js
@@ -1,11 +1,13 @@
 export const SnapRules = {
   1: {
-    title: 'Rule set 1',
-    description: 'rule set 1 description',
+    title: 'Value',
+    description:
+      'Snap when the value of the cards are the same. ex. 2 of hearts and 2 of spades.',
   },
   2: {
-    title: 'Rule set 2',
-    description: 'rule set 2 description',
+    title: 'Suit',
+    description:
+      'Snap when the suit of the cards are the same. ex. 2 of hearts and 3 of hearts.',
   },
 };
 
