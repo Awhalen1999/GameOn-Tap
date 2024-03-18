eef1316a824339081b36a8027e966754c32f3df4
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-03-18 13:47:22 -0230

    hide image until fully loaded

--- src/Pages/GameImage.jsx
+++ src/Pages/GameImage.jsx
@@ -8,20 +8,20 @@ const GameImage = ({ src, alt, ...props }) => {
   };
 
   return (
-    <>
+    <div className='relative'>
       {isLoading && (
-        <span className='loading loading-spinner text-secondary absolute inset-0 flex items-center justify-center'></span>
+        <span className='loading loading-spinner loading-large text-secondary absolute inset-0 flex items-center justify-center'></span>
       )}
       <img
         src={src}
         alt={alt}
         onLoad={handleImageLoad}
         {...props}
-        className={`w-full h-auto object-cover rounded-t-lg ${
-          isLoading ? 'hidden' : ''
+        className={`w-full h-auto object-cover rounded-t-lg transition-opacity duration-500 ${
+          isLoading ? 'opacity-0' : 'opacity-100'
         }`}
       />
-    </>
+    </div>
   );
 };
 
