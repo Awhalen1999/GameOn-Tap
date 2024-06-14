e44f7fefea54037ae26cfc390b958cd7b761ed5d
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-06-14 14:51:05 -0230

    debug log

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,12 +5,16 @@ const baseURL = 'http://localhost:3000';
 // Signup
 export async function signupUser(username, email, password) {
   const theme = 'myDark'; // Manually set the theme
+  const requestBody = JSON.stringify({ username, email, password, theme });
+
+  console.log(requestBody); // Log the request body
+
   const response = await fetch(`${baseURL}/users/signup`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ username, email, password, theme }),
+    body: requestBody,
   });
 
   if (!response.ok) {
