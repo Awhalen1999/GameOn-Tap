79f653c0326a7c67b1e478ff12e0b84ace67f7a0
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-09-05 18:36:29 -0230

    remove loaders

--- src/Pages/Games/KingsCup.jsx
+++ src/Pages/Games/KingsCup.jsx
@@ -6,7 +6,6 @@ import { getActiveRuleset, getRuleset } from '../../utils/api.js';
 import RulesetDisplay from '../../components/RulesetDisplay';
 import { useAuth } from '../../hooks/useAuth';
 import defaultRulesets from '../../components/defaultRulesets';
-import ImageLoader from '../../components/ImageLoader'; // Import the ImageLoader component
 
 const KingsCup = () => {
   const [deck, setDeck] = useState([...initialDeck]);
@@ -192,14 +191,19 @@ const KingsCup = () => {
             )}
             {drawnCards.length > 0 ? (
               <div className='mb-4 md:mb-0'>
-                <ImageLoader
+                <img
                   src={cardImages[drawnCards[0]]}
                   alt={drawnCards[0]}
+                  className='w-auto h-100 object-contain shadow-lg'
                 />
               </div>
             ) : (
               <div className='mb-4 md:mb-0 md:mr-4'>
-                <ImageLoader src={placeholderCard} alt='Placeholder' />
+                <img
+                  src={placeholderCard}
+                  alt='Placeholder'
+                  className='w-auto h-100 object-contain shadow-lg'
+                />
               </div>
             )}
           </div>
