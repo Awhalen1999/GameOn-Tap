010865c5e909d567ca87deb14143388cb5243639
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-09-10 15:45:33 -0230

    root

--- src/components/RulesetDisplay.jsx
+++ src/components/RulesetDisplay.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { IoCloseSharp } from 'react-icons/io5';
 import { FaWrench } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
 const RulesetDisplay = ({ rules, gameId }) => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+
   const gamesWithIcon = new Set([
     'KingsCup',
     'DiceRoll',
@@ -12,21 +15,26 @@ const RulesetDisplay = ({ rules, gameId }) => {
     'BountyBlast',
   ]);
 
+  const handleEditRules = () => {
+    if (user) {
+      // If the user is authenticated, navigate to Edit Rules
+      navigate(`/EditRules/${gameId}`);
+      const modal = document.getElementById('my_modal_3');
+      if (modal) {
+        modal.close();
+      }
+    } else {
+      // If not authenticated, navigate to login page
+      navigate('/login');
+    }
+  };
+
   return (
     <div className='overflow-auto pr-2'>
       <div className='flex justify-between items-center mb-4'>
         <h3 className='font-bold text-lg'>Active Ruleset</h3>
         {gamesWithIcon.has(gameId) && (
-          <button
-            className='btn btn-ghost mr-4'
-            onClick={() => {
-              navigate(`/EditRules/${gameId}`);
-              const modal = document.getElementById('my_modal_3');
-              if (modal) {
-                modal.close();
-              }
-            }}
-          >
+          <button className='btn btn-ghost mr-4' onClick={handleEditRules}>
             Edit Rules <FaWrench />
           </button>
         )}
@@ -38,20 +46,18 @@ const RulesetDisplay = ({ rules, gameId }) => {
       </div>
       {rules && (
         <div>
-          {Object.entries(rules).map(([ruleKey, rule], index, array) => {
-            return (
-              <div
-                key={index}
-                className={`mb-4 p-2 ${
-                  index !== array.length - 1 ? 'border-b border-gray-200' : ''
-                }`}
-              >
-                <h3 className='font-bold text-lg mb-2'>{rule.result}</h3>
-                <p className='font-semibold text-base'>{rule.title}</p>
-                <p className='text-sm'>{rule.description}</p>
-              </div>
-            );
-          })}
+          {Object.entries(rules).map(([ruleKey, rule], index, array) => (
+            <div
+              key={index}
+              className={`mb-4 p-2 ${
+                index !== array.length - 1 ? 'border-b border-gray-200' : ''
+              }`}
+            >
+              <h3 className='font-bold text-lg mb-2'>{rule.result}</h3>
+              <p className='font-semibold text-base'>{rule.title}</p>
+              <p className='text-sm'>{rule.description}</p>
+            </div>
+          ))}
         </div>
       )}
     </div>
