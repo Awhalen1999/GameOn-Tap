534273df0fcc5ae8703c9886f8b3b5a165a6330b
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-09-16 16:54:58 -0230

    fix login and signup sizing

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { IoClose } from 'react-icons/io5';
 import { useAuth } from '../hooks/useAuth.js';
 
 const LoginPage = () => {
@@ -7,11 +8,11 @@ const LoginPage = () => {
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
-  const { login, user, error } = useAuth();
+  const { login, user, error } = useAuth(); // Grab error from context
 
   const handleLogin = async (event) => {
     event.preventDefault();
-    await login(email, password);
+    await login(email, password); // The error state will be handled in the context
   };
 
   // Redirect user to home page if logged in
@@ -26,77 +27,91 @@ const LoginPage = () => {
   };
 
   return (
-    <div className='flex items-center h-screen lg:h-full p-6 justify-center bg-base-100'>
-      <div className='card lg:card-side bg-base-100 shadow-2xl w-full lg:w-10/12 border border-neutral rounded-none '>
-        {/* Form Section  */}
-        <div className='card-body lg:w-2/3 bg-white '>
-          <h2 className='card-title text-2xl text-black'>Account Login</h2>
-          <form className='flex flex-col space-y-4' onSubmit={handleLogin}>
-            <div className='flex flex-col space-y-1'>
-              <label
-                htmlFor='email'
-                className='text-sm font-semibold text-gray-600'
-              >
-                Email address
-              </label>
-              <input
-                type='email'
-                id='email'
-                autoFocus
-                className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className='flex flex-col space-y-1'>
-              <label
-                htmlFor='password'
-                className='text-sm font-semibold text-gray-600'
-              >
-                Password
-              </label>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                id='password'
-                className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <div className='flex items-center mt-2'>
-                <label className='label cursor-pointer'>
-                  <span className='label-text text-gray-600'>
-                    Show Password
-                  </span>
-                  <input
-                    type='checkbox'
-                    id='showPassword'
-                    checked={showPassword}
-                    onChange={togglePasswordVisibility}
-                    className='ml-2 checkbox checkbox-primary checkbox-sm'
-                  />
-                </label>
+    <div className='py-6'>
+      <div className='flex items-center h-screen lg:h-full p-6 bg-base-100 justify-center'>
+        <div className='relative flex flex-col overflow-hidden rounded-lg shadow-lg md:flex-row md:flex-1 lg:max-w-screen-md border border-neutral'>
+          <Link
+            to='/'
+            className='absolute top-2 right-2 text-2xl text-gray-500 hover:text-gray-700'
+          >
+            <IoClose />
+          </Link>
+          <div className='flex flex-col overflow-hidden rounded-lg shadow-lg md:flex-row md:flex-1 lg:max-w-screen-md'>
+            <div className='p-4 py-6 text-primary-content bg-primary md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly'>
+              <div className='my-3 text-4xl font-bold tracking-wider text-center'>
+                <h2>GameOn Tap</h2>
               </div>
+              <p className='mt-6 font-normal text-center md:mt-0'>
+                Login or create an account to access all the features of GameOn
+                Tap.
+              </p>
+              <p className='flex flex-col items-center justify-center mt-8 text-center'>
+                <span>Don't have an account?</span>
+                <Link to='/signup' className='underline font-bold'>
+                  Get Started!
+                </Link>
+              </p>
             </div>
-            {/* Display error message from context */}
-            {error && <p className='text-error'>{error}</p>}
-            <button type='submit' className='btn btn-primary w-full'>
-              Log in
-            </button>
-          </form>
-        </div>
-
-        {/* Side Section  */}
-        <div className='card-body bg-primary text-primary-content lg:w-1/3 flex flex-col  '>
-          <h2 className='text-3xl font-bold mb-6 text-center'>GameOn Tap</h2>
-          <p className='text-center'>
-            Login or create an account to access all the features of GameOn Tap.
-          </p>
-          <p className='mt-4'>
-            Don't have an account?
-            <Link to='/signup' className='underline font-bold text-black ml-1'>
-              Get Started!
-            </Link>
-          </p>
+            <div className='p-5 bg-white md:flex-1'>
+              <h3 className='my-4 text-2xl font-semibold text-black'>
+                Account Login
+              </h3>
+              <form className='flex flex-col space-y-5' onSubmit={handleLogin}>
+                <div className='flex flex-col space-y-1'>
+                  <label
+                    htmlFor='email'
+                    className='text-sm font-semibold text-gray-600'
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type='email'
+                    id='email'
+                    autoFocus
+                    className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className='flex flex-col space-y-1'>
+                  <label
+                    htmlFor='password'
+                    className='text-sm font-semibold text-gray-600'
+                  >
+                    Password
+                  </label>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    id='password'
+                    className='px-3 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <div className='flex items-center'>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text text-gray-600'>
+                        Show Password
+                      </span>
+                      <input
+                        type='checkbox'
+                        id='showPassword'
+                        checked={showPassword}
+                        onChange={togglePasswordVisibility}
+                        className='ml-2 checkbox checkbox-primary checkbox-sm'
+                      />
+                    </label>
+                  </div>
+                </div>
+                {/* Display error message from context */}
+                {error && <p className='text-error'>{error}</p>}
+                <div>
+                  <button type='submit' className='btn w-full btn-primary'>
+                    Log in
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { IoClose } from 'react-icons/io5';
 import { useAuth } from '../hooks/useAuth.js';
 
 const SignupPage = () => {
@@ -42,114 +43,134 @@ const SignupPage = () => {
   // Handle navigation when user is authenticated
   useEffect(() => {
     if (user) {
+      // Navigate to the home page when a user is authenticated
       navigate('/');
     }
-  }, [user, navigate]);
+  }, [user, navigate]); // Only triggers when 'user' state changes
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
   return (
-    <div className='flex items-center h-screen lg:h-full justify-center bg-base-100 p-6'>
-      <div className='card lg:card-side bg-base-100 shadow-2xl w-full lg:w-10/12 border border-neutral rounded-none'>
-        {/* Form Section  */}
-        <div className='card-body lg:w-2/3 bg-white'>
-          <h2 className='card-title text-2xl text-black'>Account Signup</h2>
-          <form className='flex flex-col space-y-4' onSubmit={handleSignup}>
-            <div className='flex flex-col space-y-1'>
-              <label
-                htmlFor='username'
-                className='text-sm font-semibold text-gray-600'
-              >
-                Username
-              </label>
-              <input
-                type='text'
-                id='username'
-                autoFocus
-                className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
+    <div className='py-6'>
+      <div className='flex items-center h-screen lg:h-full p-6 bg-base-100 justify-center'>
+        <div className='relative flex flex-col overflow-hidden rounded-lg shadow-lg md:flex-row md:flex-1 lg:max-w-screen-md border border-neutral'>
+          <Link
+            to='/'
+            className='absolute top-2 right-2 text-2xl text-gray-500 hover:text-gray-700'
+          >
+            <IoClose />
+          </Link>
+          <div className='flex flex-col overflow-hidden rounded-lg shadow-lg md:flex-row md:flex-1 lg:max-w-screen-md'>
+            <div className='p-4 py-6 text-primary-content bg-primary md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly'>
+              <div className='my-3 text-4xl font-bold tracking-wider text-center'>
+                <h2>GameOn Tap</h2>
+              </div>
+              <p className='mt-6 font-normal text-center md:mt-0'>
+                Login or create an account to access all the features of GameOn
+                Tap.
+              </p>
+              <p className='flex flex-col items-center justify-center mt-8 text-center'>
+                <span>Already have an account?</span>
+                <Link to='/login' className='underline font-bold'>
+                  Login!
+                </Link>
+              </p>
             </div>
-            <div className='flex flex-col space-y-1'>
-              <label
-                htmlFor='email'
-                className='text-sm font-semibold text-gray-600'
-              >
-                Email address
-              </label>
-              <input
-                type='email'
-                id='email'
-                className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+            <div className='p-5 bg-white md:flex-1'>
+              <h3 className='my-4 text-2xl font-semibold text-black'>
+                Account Signup
+              </h3>
+              <form className='flex flex-col space-y-5' onSubmit={handleSignup}>
+                <div className='flex flex-col space-y-1'>
+                  <label
+                    htmlFor='username'
+                    className='text-sm font-semibold text-gray-600'
+                  >
+                    Username
+                  </label>
+                  <input
+                    type='text'
+                    id='username'
+                    autoFocus
+                    className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className='flex flex-col space-y-1'>
+                  <label
+                    htmlFor='email'
+                    className='text-sm font-semibold text-gray-600'
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type='email'
+                    id='email'
+                    className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className='flex flex-col space-y-1'>
+                  <label
+                    htmlFor='password'
+                    className='text-sm font-semibold text-gray-600'
+                  >
+                    Password
+                  </label>
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    id='password'
+                    className='px-3 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <div className='flex flex-col space-y-1'>
+                    <label
+                      htmlFor='confirmPassword'
+                      className='text-sm font-semibold text-gray-600'
+                    >
+                      Confirm Password
+                    </label>
+                    <input
+                      type={showPassword ? 'text' : 'password'}
+                      id='confirmPassword'
+                      className='px-3 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <div className='flex items-center'>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text text-gray-600'>
+                        Show Password
+                      </span>
+                      <input
+                        type='checkbox'
+                        id='showPassword'
+                        checked={showPassword}
+                        onChange={togglePasswordVisibility}
+                        className='ml-2 checkbox checkbox-primary checkbox-sm'
+                      />
+                    </label>
+                  </div>
+                </div>
+                {error && <p className='text-error'>{error}</p>}
+                <div>
+                  <button type='submit' className='btn w-full btn-primary'>
+                    Sign up
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className='flex flex-col space-y-1'>
-              <label
-                htmlFor='password'
-                className='text-sm font-semibold text-gray-600'
-              >
-                Password
-              </label>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                id='password'
-                className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className='flex flex-col space-y-1'>
-              <label
-                htmlFor='confirmPassword'
-                className='text-sm font-semibold text-gray-600'
-              >
-                Confirm Password
-              </label>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                id='confirmPassword'
-                className='px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-primary text-black'
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-            </div>
-            <div className='flex items-center'>
-              <label className='label cursor-pointer'>
-                <span className='label-text text-gray-600'>Show Password</span>
-                <input
-                  type='checkbox'
-                  id='showPassword'
-                  checked={showPassword}
-                  onChange={togglePasswordVisibility}
-                  className='ml-2 checkbox checkbox-primary checkbox-sm'
-                />
-              </label>
-            </div>
-            {/* Display error message */}
-            {error && <p className='text-error'>{error}</p>}
-            <button type='submit' className='btn btn-primary w-full'>
-              Sign up
-            </button>
-          </form>
-        </div>
-
-        {/* Side Section */}
-        <div className='card-body bg-primary text-primary-content lg:w-1/3 flex flex-col '>
-          <h2 className='text-3xl font-bold mb-6 text-center'>GameOn Tap</h2>
-          <p className='text-center'>
-            Login or create an account to access all the features of GameOn Tap.
-          </p>
-          <p className='mt-4'>
-            Already have an account?
-            <Link to='/login' className='underline font-bold text-black ml-1'>
-              Login!
-            </Link>
-          </p>
+          </div>
         </div>
       </div>
     </div>
